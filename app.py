b4c49d64ad84a9969c88edf3e1f7c925ade37c84
Kris Bolton <krisbolton@users.noreply.github.com>
2022-02-19 09:06:59 +0000

    add method to switch publication feed

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import feedparser
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -11,9 +11,15 @@ FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 
 @app.route("/")
 @app.route("/<publication>")
-def get_news(publication="bbc"):
+def get_news():
+    query = request.args.get("publication")
+    if not query or query.lower() not in FEEDS:
+        publication = "bbc"
+    else:
+        publication = query.lower()
     feed = feedparser.parse(FEEDS[publication])
-    return render_template("index.html", articles=feed['entries'])
+    return render_template("index.html",
+                           articles=feed['entries'])
 
 
 if __name__ == '__main__':
